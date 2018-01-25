afbcc6502258d65628b5c577818b6557031de428
Bouchat Marie-Ange <32301638+bouchat-marieange@users.noreply.github.com>
2018-01-25 17:19:11 +0100

    Update index.php

--- Phase 2 - Design de la solution one-pager V2/onepage_v2/index.php
+++ Phase 2 - Design de la solution one-pager V2/onepage_v2/index.php
@@ -1,16 +1,40 @@
-<?php include "DB.php"
+<?php
+require "./DB.php";
+session_start();
 
-  $db = new DB();
-  var_dump($db);
-  echo "ets";
+if($_SERVER['REQUEST_METHOD'] == 'POST')
+{
+$db = new DB();
+$db->saveEmail();
+}
+
 ?>
 <!DOCTYPE html>
 <html>
-  <head>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
-    <h1>tets</h1>
-  </body>
+<head>
+<meta charset="utf-8">
+<title></title>
+<style media="screen">
+.message{
+background: yellow;
+}
+</style>
+</head>
+<body>
+<div class="message">
+<?= isset($_GET['message']) ? $_GET['message']: ""; ?>
+<!-- Condition ternaire: C'est une condition isset verifique que la variable existe, et si oui (condition elle va chercher le message qui a été transmis en GET dans l'url $_GET['message'], sinon elle n'affiche rien "") -->
+</div>
+<form action="" method="post">
+<input type="email" name="email">
+<input type="submit" name="" value="inscription">
+</form>
+
+</body>
+<script type="text/javascript">
+let msg = document.querySelector(".message");
+msg.addEventListener("click",function(){
+msg.style.display = "none";
+})
+</script>
 </html>
