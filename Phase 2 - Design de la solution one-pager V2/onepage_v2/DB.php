2fbfff934f8f235dcdf31be388bb4e274480bd75
GjeloshajAntoine <gjeloshaj.antoine@gmail.com>
2018-01-25 18:03:52 +0100

    intégration magique

--- Phase 2 - Design de la solution one-pager V2/onepage_v2/DB.php
+++ Phase 2 - Design de la solution one-pager V2/onepage_v2/DB.php
@@ -7,7 +7,7 @@ public $db;
 public function __construct() // On crée un fonction de connection à la DB.
 {
     try {
-        $this->db = new PDO('mysql:host=localhost;dbname=preinscription', 'root', 'user'); //objet PDO contenant les infos de connection à la DB (nom DB, username et mot de passe)
+        $this->db = new PDO('mysql:host=localhost;dbname=preinscription', 'root', 'root'); //objet PDO contenant les infos de connection à la DB (nom DB, username et mot de passe)
         $this->db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     } catch (Exception $exception) {
@@ -17,20 +17,18 @@ public function __construct() // On crée un fonction de connection à la DB.
 
 public function saveEmail()// prépare insertion du mail que l'on va recevoir de l'utilisateur
 {
-  if($_SERVER['REQUEST_METHOD'] == 'POST')// On vérifie si le mail à bien été envoyé avec la méthode POST
-  {
-    $mail = $_POST['email'];
-    $req = $this->db->prepare("insert into email (email) value(?)");//value(?) cela correspond au nombre d'élément que l'on veut insérer. Le nombre de point d'interrogation est le même que le nombre d'élément que l'on veut insérer. Ici 1 seul point car juste élement = adresse mail.
-    $req->execute([$mail]);//La requête va aller chercher $mail qui correspond au premier point d'interrogation. Attention l'ordre des éléments quand il y en a plusieurs est important premier point interrogation correspond au premier element , etc...
-
-    if($req->rowCount())
+    if($_SERVER['REQUEST_METHOD'] == 'POST')// On vérifie si le mail à bien été envoyé avec la méthode POST
     {
-      header('location:index.php?message=Bravo, Votre inscription est validée');
+        $mail = $_POST['email'];
+        $req = $this->db->prepare("insert into email (email) value(?)");//value(?) cela correspond au nombre d'élément que l'on veut insérer. Le nombre de point d'interrogation est le même que le nombre d'élément que l'on veut insérer. Ici 1 seul point car juste élement = adresse mail.
+        $req->execute([$mail]);//La requête va aller chercher $mail qui correspond au premier point d'interrogation. Attention l'ordre des éléments quand il y en a plusieurs est important premier point interrogation correspond au premier element , etc...
+
+        if($req->rowCount())
+        {
+           $_SESSION['message'] =  "Bravo, Votre inscription est validée";
+        }
     }
-  }
-}
 }
 
+}
 ?>
-
-
--- Phase 2 - Design de la solution one-pager V2/onepage_v2/index.php
+++ Phase 2 - Design de la solution one-pager V2/onepage_v2/index.php
@@ -1,40 +1,95 @@
 <?php
-require "./DB.php";
-session_start();
-
-if($_SERVER['REQUEST_METHOD'] == 'POST')
-{
-$db = new DB();
-$db->saveEmail();
-}
-
+  require "./DB.php";
+  session_start();
+  if (isset($_SESSION['message'])) {
+  session_destroy();
+  }
+  if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+  $db = new DB();
+  $db->saveEmail();
+  }
 ?>
 <!DOCTYPE html>
 <html>
-<head>
-<meta charset="utf-8">
-<title></title>
-<style media="screen">
-.message{
-background: yellow;
-}
-</style>
-</head>
-<body>
-<div class="message">
-<?= isset($_GET['message']) ? $_GET['message']: ""; ?>
-<!-- Condition ternaire: C'est une condition isset verifique que la variable existe, et si oui (condition elle va chercher le message qui a été transmis en GET dans l'url $_GET['message'], sinon elle n'affiche rien "") -->
-</div>
-<form action="" method="post">
-<input type="email" name="email">
-<input type="submit" name="" value="inscription">
-</form>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="style.css">
+    <style media="screen">
+      .messageDiv {
+        background: #2b860c;
+        width:300px;
+        padding:5px;
+        border-radius: 2px;
+      }
+      .message{
+        font-family:Arial;
+        color: white;
+        text-align: center;
+      }
+    </style>
+  </head>
+  <body>
+  <div class="messageDiv">
+    <p class="message"><?= isset($_SESSION['message']) ? $_SESSION['message'] : ""; ?></p>
+        <!-- Condition ternaire: C'est une condition isset verifique que la variable existe, et si oui (condition elle va chercher le message qui a été transmis en GET dans l'url $_GET['message'], sinon elle n'affiche rien "") -->
+  </div>
+    <div class="un">
+      <div class="centré">
+        <h2>Une association à votre écoute</h2>
+        <p>
+          Vous désirez être tenu au courant des activités organisées par notre association?
+        </p>
+        <p>
+          Inscrivez-vous à notre newsletter
+        </p>
+        <form class="" action="index.html" method="post">
+          <input type="text" name="" value="" placeholder="C'est ta mère le placeholder">
+          <input type="submit" name="" value="INSCRIPTION">
+        </form>
+      </div>
+    </div>
+    <div class="deux">
+      <h1>Entendre des voix</h1>
+      <h2>REV - BELGIUM</h2>
+      <p class="sous-titre">
+        REV-Belgium (Réseau des Entendeurs de Voix) est un réseau d'aide et d'information pour les personnes qui entendent des voix et/ou font l'expérience de perceptions inhabituelles.
+      </p>
+      <div class="blocks">
+        <div class="block">
+          <i class="fa i fa-users" aria-hidden="true"></i>
+          <h3>Groupes d'entraides</h3>
+          <p>
+            Nos groupes d'entraide permettent à ceux qui entendent des voix de sortir de leur isolement pour partager en toute liberté et sans préjugé leurs expériences.
+          </p>
+        </div>
+        <div class="block">
+          <i class="i fa  fa-address-book-o"  aria-hidden="true"></i>
+          <h3>Evènements</h3>
+          <p>
+            Les congrès internationaux d'entendeur de voix permet un visibilité par delà les frontières de cette expérience encore taboue.
+          </p>
+        </div>
+        <div class="block">
+          <i class="fa i fa-bullhorn"  aria-hidden="true"></i>
+          <h3>Information</h3>
+          <p>
+            L'information, le soutien et la compréhension offert à ceux qui entendent des voix à déja permis à de nombreuses personnes de retrouver leur identité et de reprendre leur vie en mains.
+          </p>
+        </div>
+    </div>
 
-</body>
-<script type="text/javascript">
-let msg = document.querySelector(".message");
-msg.addEventListener("click",function(){
-msg.style.display = "none";
-})
-</script>
+      <span>Copyright - REV - 2018 -  <a href="#">copyright</a></span>
+    </div>
+    <script type="text/javascript">
+    let msg = document.querySelector(".messageDiv");
+    msg.addEventListener("click", function () {
+    msg.style.display = "none";
+    })
+    setTimeout(() => {
+    msg.style.display = "none";
+    }, 8000)
+    </script>
+  </body>
 </html>
