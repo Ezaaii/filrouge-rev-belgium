fdca3acba9cde7d1d566b44717038d07874dc3dc
Bouchat Marie-Ange <32301638+bouchat-marieange@users.noreply.github.com>
2018-01-26 15:55:24 +0100

    DB optimisé

--- Phase 2 - Design de la solution one-pager V2/onepage_v2/DB.php
+++ Phase 2 - Design de la solution one-pager V2/onepage_v2/DB.php
@@ -24,21 +24,34 @@ class DB // Création d'une classe pour se connecter à la DB
         {
             session_start();
             $mail = isset($_POST['email']) ? $_POST['email'] : "";
-            if ($mail != "") {
+            if (filter_var($mail, FILTER_VALIDATE_EMAIL))
+            // Vérification en php que les caractères entré par l'utilisateur corresponde bien à un email
+            {
                 $req = $this->db->prepare("SELECT * FROM email where email = (?)");
                 $req->execute([$mail]);
                 $nbRow = count($req->fetchAll());
 
-                if ($nbRow > 0) {
+                if ($nbRow > 0)
+                {
                     $_SESSION['messageAlt'] = "Cette adresse mail est déja enregistrée";
-                } else {
+                }
+                else
+                {
                     $mail = isset($_POST['email']) ? $_POST['email'] : "";
                     $req = $this->db->prepare("insert into email (email) value (?)");//value(?) cela correspond au nombre d'élément que l'on veut insérer. Le nombre de point d'interrogation est le même que le nombre d'élément que l'on veut insérer. Ici 1 seul point car juste élement = adresse mail.
                     $req->execute([$mail]);//La requête va aller chercher $mail qui correspond au premier point d'interrogation. Attention l'ordre des éléments quand il y en a plusieurs est important premier point interrogation correspond au premier element , etc...
                     $_SESSION['message'] = "Bravo, Votre inscription est validée";
                 }
             }
+            else
+            {
+              // Message qui sera affiché si les caractères entrés par l'utilisateur ne corresponde pas à un email
+              $_SESSION['message'] = "Veuillez entrer un email correct !";
+
+            }
+
         }
+
     }
 
 }
