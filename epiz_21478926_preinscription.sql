a05b7acc5fe6d1680fe85f3e9f3f9a2c29c43755
Bouchat Marie-Ange <32301638+bouchat-marieange@users.noreply.github.com>
2018-01-26 16:09:25 +0100

    Ajout de l'export db

---
+++ epiz_21478926_preinscription.sql
@@ -0,0 +1,105 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.8.2
+-- http://www.phpmyadmin.net
+--
+-- Host: sql305.epizy.com
+-- Generation Time: Jan 26, 2018 at 10:08 AM
+-- Server version: 5.6.35-81.0
+-- PHP Version: 5.3.3
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `epiz_21478926_preinscription`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `email`
+--
+
+CREATE TABLE IF NOT EXISTS `email` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=63 ;
+
+--
+-- Dumping data for table `email`
+--
+
+INSERT INTO `email` (`id`, `email`) VALUES
+(1, 'somao@test.com'),
+(2, 'somao@test.com'),
+(3, 'somao@test.com'),
+(4, 'somao@test.com'),
+(5, 'test@gmail.com'),
+(6, 'xda@gmailcom'),
+(7, 'xda@gmail.com'),
+(8, 'test@gmail.be'),
+(9, 'test@gmail.be'),
+(10, 'test@test.com'),
+(11, 'test@test.com'),
+(12, 'test@test.com'),
+(13, 'test@test.com'),
+(14, 'test@test.com'),
+(15, 'test@test.com'),
+(16, 'test@gmail.com'),
+(17, 'test@gmail.com'),
+(18, 'test@gmail.com'),
+(19, 'test@gmail.com'),
+(20, 'test@gmail.com'),
+(21, 'test@gmail.com'),
+(22, 'test@gmail.com'),
+(23, 'test@gmail.com'),
+(24, 'test@gmail.com'),
+(25, 'test@gmail.com'),
+(26, 'test@gmail.com'),
+(27, 'test@gmail.com'),
+(28, 'test@gmail.com'),
+(29, 'tets@test.com'),
+(30, 'test@gmail.com'),
+(31, 'test@test.com'),
+(32, 'test@test.com'),
+(33, 'kef@gal.cwq'),
+(34, '1test@test.com'),
+(35, '1test@test.com'),
+(36, 'somao@out.com'),
+(37, 'test@test.com'),
+(38, 'test@test.com'),
+(39, 'test@test.com'),
+(40, 'test@test.com'),
+(41, 'test@test.com'),
+(42, 'somao@out3.comx'),
+(43, 'somao@out.xx'),
+(44, ''),
+(45, 'test@test.comwww'),
+(46, 'test@test.er'),
+(47, 'test@test.compo'),
+(48, 'kev@out.coxm'),
+(49, 'test@teeest.com'),
+(50, 'kef@gal.ca'),
+(51, 'viy@vvv.com'),
+(52, 'keey@vvv.com'),
+(53, 'jcuenco@mail.bex'),
+(54, 'jcuenco@mail.beg'),
+(55, 'svvv@out.com'),
+(56, 'rsomao@out.com'),
+(57, 'somao@out.comet'),
+(58, 'somao@out.comw'),
+(59, 'alexandre@becode.org'),
+(60, 'testing@gmail.com'),
+(61, 'opieu@poieru.com'),
+(62, 'oiazeu@gmail.com');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
